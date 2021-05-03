acda93bf089809d85513d2a94d35fcc78e630692
hiukongDan <ifsoboy9148@hotmail.com>
2021-05-03 09:23:40 +0800

    test php

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Title -->
+	<title>
+	Hiukong Dan - A site
+	</title>
+	<!-- Phaser -->
+    <script src="https://cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser-arcade-physics.min.js"></script>
+	
+	<!-- CSS -->
+	<link rel="stylesheet" href="static/css/skeleton/normalize.css">
+	<link rel="stylesheet" href="static/css/skeleton/skeleton.css">
+	<link rel="stylesheet" href="static/css/skeleton/custom.css">
+	
+	<!-- Meta -->
+	<meta charset="utf-8" />
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="twelve columns">
+				<?php echo '<h2>A presentation of Phaser - h5 game engine</h2>'; ?>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="twelve columns">
+				<script src="static/script/game_demon.js"></script>
+			</div>
+		</div>
+
+	</div>
+
+
+</body>
+</html>
